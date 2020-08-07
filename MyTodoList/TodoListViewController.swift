fda83d3827a3488bb3ba11ddcf967217cab304be
WuHouxuan <48287340+wuhouxxxx@users.noreply.github.com>
2020-08-07 11:22:10 +0800

    v0.2, editing checkmark

--- MyTodoList/TodoListViewController.swift
+++ MyTodoList/TodoListViewController.swift
@@ -28,5 +28,15 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
