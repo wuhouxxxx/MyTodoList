21668d241e0f8a9e6883ec4ea8ce9004856990f6
WuHouxuan <48287340+wuhouxxxx@users.noreply.github.com>
2020-08-07 10:47:56 +0800

    v0.1

---
+++ MyTodoList/TodoListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by mac on 2020/8/7.
+//  Copyright © 2020 WuHouxuan. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["吃药", "喝酒", "烫头"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+//        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+}
+
